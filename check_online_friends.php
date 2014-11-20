92150b709fc11de0a65ea1bdcd4f6534c0445828
Danil Mikhailov <danil@etersoft.ru>
2014-11-20 22:03:37 +0300

    Added users.get

--- check_online_friends.php
+++ check_online_friends.php
@@ -2,12 +2,12 @@
 
 class OnlineHistory
 {
-//	var $id="";
-	var $url="https://api.vk.com/method/friends.get?user_id=749972";
+	private $id="749972";
 	private $json;
 	private $dbhandle;
+	private $vkapi_token='cd7781010610415dd8d3a039d5cbaedc0309b19ff19c58d3e8ab67294fa7ab85ed5d29837bedd1a05758a';
 
-	function get_data($url) { 
+	function send_req($url){
 	     $ch = curl_init(); 
 	     $timeout = 5; 
 	     curl_setopt($ch, CURLOPT_URL, $url);
@@ -15,7 +15,19 @@ class OnlineHistory
 	     curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
 	     $data = curl_exec($ch);
 	     curl_close($ch);
+// 	     print $data;
 	     return $data;
+	}
+	
+	function get_friends() { 
+	     $url="https://api.vk.com/method/friends.get?user_id=749972"; //https://api.vk.com/method/friends.get?user_id=$id\&access_token=$vkapi_token
+	     return $this->send_req($url);
+	}
+
+	function get_online() { 
+	     $url="https://api.vk.com/method/users.get?user_ids=" . $this->get_friends() . "," . $this->id . ",\&fields=online,photo_50,\&lang=en";
+// 	     print $url;
+	     return $this->send_req($url);
 	}
 
 	function connect()
@@ -44,15 +56,22 @@ class OnlineHistory
 
 	function download()
 	{	
-		$json = $this->get_data($this->url);
-		print($json);
+// 		$json = $this->get_friends($this->url);
+ 		$json = $this->get_online();
+// 		print($json);
 		$obj = json_decode($json);
-		print_r($obj);
-		$this->connect();
+// 		print_r($obj);
+// 		$this->connect();
 		foreach( $obj->response as $value)
 		{
-			$this->save_to_db($value);
-//			print($value );
+//			$this->save_to_db($value);
+			print_r($value->uid . " ");
+			print_r($value->online . " ");
+			print_r($value->first_name . " ");
+			print_r($value->last_name . " ");
+			print_r($value->photo_50. " ");
+			print("\r\n");
+// 			print($value->online_mobile);
 		}
 
 	}
@@ -60,7 +79,8 @@ class OnlineHistory
 
 }
 	
-(new OnlineHistory() )-> download();
+$myOnlineHistiry = new OnlineHistory();
+$myOnlineHistiry->download();
 
 // connect on builder64
 //create table space_table (user_name varchar(255), user_score int);
