3d610d08d5753815327507dad81cb25b94b2a4ce
Danil Mikhailov <danil@altlinux.org>
2015-10-23 05:45:32 +0300

    Added previous dates

--- html_js_php/user_charts.php
+++ html_js_php/user_charts.php
@@ -1,9 +1,5 @@
-<!DOCTYPE html>
-<html>
-
-
 <?php 
-include './online_table.php';
+include '/home/danil/Projects/vkonline/html_js_php/online_table.php';
 
 class OnlineHistoryCharts extends OnlineHistory{
 	
@@ -89,69 +85,13 @@ graph_by_ids();
 <!--<script src="http://code.highcharts.com/highcharts.js"></script>-->
 
 
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="../../favicon.ico">
 
-    <title>How much your friend online in vk</title>
+<?php
+include '/home/danil/Projects/vkonline/html_js_php/start.php';
+?>
 
-    <!-- Bootstrap core CSS -->
-    <link href="../../dist/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom styles for this template -->
-    <link href="dashboard.css" rel="stylesheet">
-
-    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
-    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
-    <script src="../../assets/js/ie-emulation-modes-warning.js"></script>
-
-    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-  </head>
-
-  <body>
-
-    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-      <div class="container-fluid">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="/">VKonline</a>
-        </div>
-       <nav id="bs-navbar" class="collapse navbar-collapse">
-      <ul class="nav navbar-nav">
-      
-      
-      
-        <li>
-          <a href="http://vk.pr.etersoft.ru/all/13.10.15.html">13.10.15</a>
-        </li>
-      </ul>
-    </nav>
-
-      </div>
-    </div>
-    <div class="container-fluid">
-      <div class="row">
-          <h2 class="sub-header">
-          <?php  date_default_timezone_set('UTC'); echo date("d.m.y"); ?>
-          
-          </h2>
 <div id="container" style="min-width: 310px; height: 400px; margin: 0 auto"></div>
-    <script type="text/javascript">
+<script type="text/javascript">
 
 jQuery.noConflict();
 
@@ -207,17 +147,6 @@ theme = 'default';
 jQuery(document).ready(function(){jQuery("#view-menu").click(function(e){jQuery("#wrap").toggleClass("toggled")}),jQuery("#sidebar-close").click(function(e){jQuery("#wrap").removeClass("toggled")}),jQuery(document).keydown(function(e){var t;"INPUT"!=e.target.tagName&&(39==e.keyCode?t=document.getElementById("next-example"):37==e.keyCode&&(t=document.getElementById("previous-example")),t&&(location.href=t.href))}),jQuery("#switcher-selector").bind("change",function(){var e=jQuery(this).val();return e&&(window.location=e),!1})});
 </script>
 
-        </div>
-      </div>
-    </div>
-
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-    <script src="../../dist/js/bootstrap.min.js"></script>
-    <script src="../../assets/js/docs.min.js"></script>
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
-  </body>
-</html>
+<?php
+include '/home/danil/Projects/vkonline/html_js_php/end.php';
+?>
