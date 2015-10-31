1e0cc12ca76ad2f02f7cfa3ec099a058719ade03
Danil Mikhailov <danil@altlinux.org>
2015-10-31 22:22:30 +0300

    Refactor js charts from one php file into charts files

---
+++ chart_one.php
@@ -0,0 +1,56 @@
+<script type="text/javascript">
+jQuery.noConflict();
+
+var example = 'areaspline', 
+theme = 'default';
+(function($){ // encapsulate jQuery
+    $(function () {
+    $('#container').highcharts({
+        chart: {
+            type: 'areaspline'
+        },
+        title: {
+            text: 'Когда и сколько вы были онлайн за этот год'
+        },
+        legend: {
+            layout: 'vertical',
+            align: 'left',
+            verticalAlign: 'top',
+            x: 150,
+            y: 100,
+            floating: true,
+            borderWidth: 1,
+            backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'
+        },
+        xAxis: {
+            categories: 
+            categories,
+            //[1,2],
+            title: {
+                text: 'Время дня'
+            }
+        },
+        yAxis: {
+            title: {
+                text: 'Часов онлайн'
+            }
+        },
+        tooltip: {
+            shared: true,
+            valueSuffix: ' часов'
+        },
+        credits: {
+            enabled: false,
+        },
+        plotOptions: {
+            areaspline: {
+                fillOpacity: 0.5
+            }
+        },
+        series: series_activity_by_user
+    });
+});
+})(jQuery);
+jQuery(document).ready(function(){jQuery("#view-menu").click(function(e){jQuery("#wrap").toggleClass("toggled")}),jQuery("#sidebar-close").click(function(e){jQuery("#wrap").removeClass("toggled")}),jQuery(document).keydown(function(e){var t;"INPUT"!=e.target.tagName&&(39==e.keyCode?t=document.getElementById("next-example"):37==e.keyCode&&(t=document.getElementById("previous-example")),t&&(location.href=t.href))}),jQuery("#switcher-selector").bind("change",function(){var e=jQuery(this).val();return e&&(window.location=e),!1})});
+</script>
+
