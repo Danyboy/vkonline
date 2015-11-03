caf5348648d7d2e5514c491c01c57b95842d2b39
Danil Mikhailov <danil@altlinux.org>
2015-11-03 20:06:48 +0300

    Added datepicker to index.php

--- index_js.php
+++ index_js.php
@@ -4,7 +4,9 @@ include '/home/danil/Projects/vkonline/start.php';
 <!-- Main jumbotron for a primary marketing message or call to action -->
     <div class="jumbotron">
       <div id="main_info" class="container">
-        <p> Привет! Здесь можно узнать сколько времени вы провели онлайн вконтакте <?php echo $myOnlineHistiry->get_correct_date($_GET['d']); ?> или в <a href="u?users=[339229,749972,42606657]&d=24.02.15">любую другую дату</a>.
+        <p> Привет! Здесь можно узнать сколько времени вы провели онлайн вконтакте <input class="datepicker" data-date-format="dd.mm.yy" size="5"> 
+    	    или в <a href="u?users=[339229,749972,42606657]" onclick="my_date=$('.datepicker').val(); location.href=this.href+'&d='+my_date;return false;">
+    	    любую другую дату</a>.
             Есть поминутная статистика и <a href="u?users=[339229,749972,42606657]&d=">красочные графики</a>. 
         </p>
 	<div style="width: 10%; margin: 0 auto;">
@@ -24,6 +26,14 @@ include '/home/danil/Projects/vkonline/middle.php';
 $myOnlineHistiry->show_today_online_users($_GET['d']);
 ?>
 
+<script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.5.0/js/bootstrap-datepicker.js"></script>
+
+<script language="javascript">
+//$('.datepicker').datepicker({	endDate: '+0d',      autoclose: true });
+$(".datepicker").datepicker("setDate", new Date());
+</script>
+
 <script language="javascript">
 VK.init({
         apiId: 5121918
@@ -35,7 +45,7 @@ function authInfo(response) {
         document.getElementById('login_button').style.display = 'none';
         change_info_for_logged(response.session.mid);
     } else {
-    alert('not auth');
+	//alert('not auth');
   }
 }
 
