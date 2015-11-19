cbbab9ff9d147af8e93dddce2829342773a1f4dd
Danil Mikhailov <danil@altlinux.org>
2015-11-20 00:13:51 +0300

    Fix add follower

--- includes/add_follower.php
+++ includes/add_follower.php
@@ -1,4 +1,5 @@
 <?php 
+include '../online_table.php';
 
 class OnlineHistoryFollower extends OnlineHistory{
 
@@ -6,11 +7,15 @@ class OnlineHistoryFollower extends OnlineHistory{
 
 		$check_user_query = "SELECT COUNT(id) FROM followers WHERE id={$user};";
                 $insert_follower_query = "INSERT INTO followers (id) VALUES ($user);";
-		if (user_non_exists($check_user_query)){
+		if ($this->user_non_exists($check_user_query)){
+			//echo $insert_follower_query;
 			$this->query_to_json($insert_follower_query);
 		}
 	}
 
 }
 
+$user = $_POST['user'];
+$myOnlineHistiry = new OnlineHistoryFollower();
+$myOnlineHistiry->add_follower($user);
 ?>
