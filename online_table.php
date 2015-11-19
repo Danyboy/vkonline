f4de5cbc87753826db97b332580b5542e4941eb4
Danil Mikhailov <danil@altlinux.org>
2015-11-19 21:59:34 +0300

    Change pgsql connection parametrs to variable

--- online_table.php
+++ online_table.php
@@ -56,7 +56,7 @@ class OnlineHistory
 		$my_db = "vk";
 
 		//connection to the database
-		$this->dbconn = pg_connect("dbname=vk user=root password=***REMOVED***")
+		$this->dbconn = pg_connect("dbname={$my_db} user={$username} password={$password}")
 		  or die("Unable to connect to PostgreSQL");
 		//echo "Connected to PostgreSQL<br>";
 	}
@@ -142,45 +142,6 @@ class OnlineHistory
 	        }
 	}
 
-	function get_users_compatibility($user){
-	
-                $count_query = "
-			SELECT id, link, name, count, coef
-			FROM (
-			    SELECT my_users.user_id, COUNT (*) AS count, COUNT(*)::float / user_coef.norm AS coef
-			    FROM user_online LEFT JOIN user_online AS my_users
-			        ON my_users.status = user_online.status
-			        INNER JOIN (SELECT user_id, COUNT(*) AS norm
-				FROM user_online
-				GROUP BY user_id) AS user_coef
-				    ON user_coef.user_id = my_users.user_id
-			    WHERE user_online.user_id = {$user}
-			    GROUP BY user_online.user_id, my_users.user_id, user_coef.norm
-			    ORDER BY count DESC
-			) AS my_comp
-			JOIN users 
-			ON (my_comp.user_id = id);
-				";
-
-		return $this->query_to_json($count_query);
-	}
-
-	function show_users_compatibility($user){
-		foreach (json_decode($this->get_users_compatibility($user)) as $row) {
-		    $num = number_format($row[4], 2, '.', '');
-		    $time_together = number_format($row[3] / 12, 0, '.', '');
-		    echo "<tr>
-			<td><input type='checkbox' name='mycheckbox' value='{$row[0]}'></td>
-			<td><a href='http://vk.com/id{$row[0]}'>
-			    <img src='{$row[1]}' alt='$row[2]'></a>
-			    <a href='./u?users=[{$row[0]},385525,690765]&d={$my_date}'>
-			    {$row[2]}	<img src='Chart-icon.png' alt='$row[2]' align='right'></a></td>
-			<td>{$num}</td>
-			<td>{$time_together} ч</td>
-		      </tr>";
-	        }
-	}
-	
 	function save_online_users($value){
 		if (strcmp("{$value->online}", "0") !== 0){
             	    $this->save_to_db($value);
