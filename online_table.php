aceca613c27a06b019d0378de024188369ea5296
Danil Mikhailov <danil@altlinux.org>
2015-11-26 20:03:45 +0300

    Compatibility: change links, view, default id

--- online_table.php
+++ online_table.php
@@ -146,7 +146,7 @@ class OnlineHistory
 			<td><input type='checkbox' name='mycheckbox' value='{$row[0]}'></td>
 			<td><a href='http://vk.com/id{$row[0]}'>
 			    <img src='{$row[1]}' alt='$row[2]'></a>
-			    <a href='./u?users=[{$row[0]},385525,749972]&d={$my_date}'>
+			    <a href='./u?users=[{$row[0]},$this->id,749972]&d={$my_date}'>
 			    {$row[2]}	<img src='Chart-icon.png' alt='$row[2]' align='right'></a></td>
 			<td><a href='c?u={$row[0]}'>
 			    {$my_time} <img src='includes/heart_25.png' alt='$row[2]' alight='right'></a></td>
