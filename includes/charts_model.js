d4a885860af299eb33c0ece892ad51eaa5b0d6a5
Danil Mikhailov <danil@altlinux.org>
2015-12-25 19:03:56 +0300

    Extrude js locations to js from php

--- includes/charts_model.js
+++ includes/charts_model.js
@@ -184,3 +184,26 @@ function add_logged_user(id) {
         }
     }
 }
+
+function frame_killer(){
+    if(self == top) {
+	document.documentElement.style.display = 'block'; 
+    } else {
+	top.location = self.location; 
+    }
+}
+
+function set_user_url(id){
+	//frame_killer();
+	if ( ! (location.search.indexOf("?u="+id) > -1)){
+	    document.location.assign("/?u=" + id);
+	}
+}
+
+function stateChange(id) {
+    setTimeout(function () {
+	    console.log("runned");
+	    set_user_url(id);
+    	    change_info_for_logged(id);
+    }, 2000);
+}
