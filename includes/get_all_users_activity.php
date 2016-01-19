82eed197e92693c3846a2c1f500ca202c2871ffe
Danil Mikhailov <danil@altlinux.org>
2016-01-19 22:44:33 +0300

    Charts: enable ajax version

---
+++ includes/get_all_users_activity.php
@@ -0,0 +1,16 @@
+<?php 
+include 'get_charts.php';
+$myOnlineHistiry = new OnlineHistoryCharts();
+
+$users = json_decode($_GET['users']);
+$my_date = $_GET['d'];
+$current_user = $_GET['u'];
+
+$all_data = $myOnlineHistiry->get_activity_by_user($users, $myOnlineHistiry->get_current_id($current_user));
+$my_names = $myOnlineHistiry->get_current_users_name($users, $myOnlineHistiry->get_current_id($current_user));
+
+echo "{ 
+    \"data\": $all_data,
+    \"names\": $my_names
+    }";
+?>
